7792cca3356f41998225ae4cb1594ee2b14ab0a9
Waphyoo <nonny2246@gmail.com>
2023-12-17 05:36:56 +0700

    okok

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.util.Collection;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        HashMapSet map=new HashMapSet();
+        HashMapSet map2=new HashMapSet();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        map.add(1);
+        map.add("banana");
+        map.add(true);
+        map2.add(2);
+        System.out.println(map.contains("banana"));
+        System.out.println(map.contains(1));
+
+        System.out.println(map.contains(2));
+
     }
 }
